9b948afc77052e030ecc539d48eb235a7eedbab3
ABAi <80210850+ABai-fefeder@users.noreply.github.com>
2021-08-01 17:24:52 +0800

    blog-add

---
+++ js/blog.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  
+  $(".back a").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 600);
+  });
+
+  $('#icon').hover(function () {
+      // over
+      $(this).addClass("animate__animated animate__flash");
+    }, function () {
+      // out
+      $(this).removeClass("animate__animated animate__flash");
+    }
+  );
+
+  $('.card').hover(function () {
+      // over
+      $(this).addClass('animate__animated animate__pulse');
+    }, function () {
+      // out
+      $(this).removeClass('animate__animated animate__pulse');
+    }
+  );
+
+  $('.navbar-nav .nav-link').hover(function () {
+    // over
+    $(this).addClass("animate__animated animate__jello");
+  }, function () {
+    // out
+    $(this).removeClass("animate__animated animate__jello");
+  }
+  );
+
+});
+
+var icon = document.getElementById("icon")
+icon.onclick = function(){
+  document.body.classList.toggle("dark-mode")
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,44 @@
-$(document).ready(function() {
-
-  $(".dropdown").click(function () { 
-    $(".dropdown-menu").addClass("animate__animated animate__bounceInDown");
+$(document).ready(function () {
+  
+  $(".back a").click(function (e) {
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 600);
   });
+
+  $('#icon').hover(function () {
+      // over
+      $(this).addClass("animate__animated animate__flash");
+    }, function () {
+      // out
+      $(this).removeClass("animate__animated animate__flash");
+    }
+
+
+  );
+
+  $('.card').hover(function () {
+      // over
+      $(this).addClass('animate__animated animate__pulse');
+    }, function () {
+      // out
+      $(this).removeClass('animate__animated animate__pulse');
+    }
+  );
+
+  $('.nav-link').hover(function () {
+    // over
+    $(this).addClass("animate__animated animate__jello");
+  }, function () {
+    // out
+    $(this).removeClass("animate__animated animate__jello");
+  }
+  );
+
+
+
 });
+
+var icon = document.getElementById("icon")
+icon.onclick = function(){
+  document.body.classList.toggle("dark-mode")
+}
